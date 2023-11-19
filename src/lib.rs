5ad36eb6552e0a230c2c6a2eda4d188372b27b0a
Julius Janeliunas <julius.janeliunas@gmail.com>
2023-11-19 14:45:36 +0200

    Remove useless imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#[allow(unused_imports)]
 mod loom;
 
 mod park;
--- src/loom.rs
+++ src/loom.rs
@@ -13,7 +13,6 @@ macro_rules! spread_attr {
 spread_attr! {
 #[cfg(not(loom))]
 
-pub(crate) use core::sync::atomic::AtomicUsize;
 pub(crate) use std::sync::{Condvar, Mutex, MutexGuard};
 pub(crate) use core::cell::Cell;
 }
@@ -21,7 +20,6 @@ pub(crate) use core::cell::Cell;
 spread_attr! {
 #[cfg(loom)]
 
-pub(crate) use loom::sync::atomic::AtomicUsize;
 pub(crate) use loom::sync::{Condvar, Mutex, MutexGuard};
 pub(crate) use loom::cell::Cell;
 }
